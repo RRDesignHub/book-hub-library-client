cdcfee5073362054f44c18b098a69024f7d424ec
Ripanul Alam Ridoy <ripanulalam8@gmail.com>
2025-01-01 13:31:01 +0600

    return book bnt functionality

--- src/Pages/BorrowedBooks.jsx
+++ src/Pages/BorrowedBooks.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
-import { format } from "date-fns";
+import { format, toDate } from "date-fns";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import Loading from "../components/Loading";
+import toast from "react-hot-toast";
 
 export const BorrowedBooks = () => {
   const { user } = useContext(AuthContext);
   const [books, setBooks] = useState([]);
-
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
+    setLoading(true);
     handleAllBooks();
+    setLoading(false);
   }, []);
 
   const handleAllBooks = async () => {
@@ -17,16 +20,36 @@ export const BorrowedBooks = () => {
       `${import.meta.env.VITE_API_URL}/borrowedBooks/${user?.email}`
     );
     setBooks(data);
+    
   };
+
+  const handleReturn = async (id, bookId, userMail) => {
+    try {
+      const { data } = await axios.delete(
+        `${import.meta.env.VITE_API_URL}/returnBook/${id}/${bookId}/${userMail}`
+      );
+      if(data.deletedCount){
+        toast.success("Successfully returned the book!!!")
+        handleAllBooks()
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+  
   return (
     <>
-      {books.length == 0 ? (
+      {loading ? (
         <Loading></Loading>
       ) : (
         <div className="container w-11/12 mx-auto py-8">
-          <h1 className="text-2xl md:text-3xl text-center font-bold text-[#334155] mb-6">
+          {
+            books.length == 0 ? 
+            <h1 className="text-2xl md:text-3xl text-center font-bold text-[#334155] mb-6">You haven't borrowed any books yet!!!</h1>:
+            <h1 className="text-2xl md:text-3xl text-center font-bold text-[#334155] mb-6">
             My Borrowed Books
           </h1>
+          }
 
           {/* List of Borrowed Books */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -67,7 +90,9 @@ export const BorrowedBooks = () => {
                 {/* Return Button */}
                 <button
                   className="w-full py-2 px-4 bg-[#334155] hover:bg-[#9AA6B2] text-white font-medium rounded-md transition"
-                  // onClick={() => handleReturn(book.id)}
+                  onClick={() =>
+                    handleReturn(book?._id, book?.bookId, book?.userEmail)
+                  }
                 >
                   Return Book
                 </button>
